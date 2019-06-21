d468380abd3140999fa80b538ee80bf386f50ec9
devrajdunna <devrajdunna17@exgmail.com>
2019-06-21 16:41:23 +0530

    Upload Success

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ApplicationRecord
-	has_attached_file :image, styles: { large:"500x600>", medium: "250x300>", thumb: "80x100>" },
-					  :default_url => "/images/ytrlg.png",
-					  :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
-	validates_attachment_presence :image
-	validates_attachment_content_type :image, :content_type =>['image/jpeg', 'image/png']
+	
+	has_attached_file :image
 
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
+	                 
+
+
