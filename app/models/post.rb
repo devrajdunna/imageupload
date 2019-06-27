96b3097ec91077419bd4d2a3cf5f17ec76d628fd
devrajdunna <devrajdunna17@exgmail.com>
2019-06-27 19:58:07 +0530

    Added New Navigation Bar Style in  Layouts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ApplicationRecord
 	
-	has_attached_file :image
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/images/ytrlg.png"
+	# styles: { large:'400×400>', medium='300×250>', thumb:'100×80#'}
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-end
-	                 
+end          
+	               
 
 
